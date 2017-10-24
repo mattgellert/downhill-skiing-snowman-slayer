4b5fb09f32f2382df7967281acfa00d57ff5d307
mattgellert <mattgellert@gmail.com>
2017-10-23 21:16:35 -0400

    api working

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :top_score, :snowmen, :game_count
+  attributes :id, :username, :top_score, :total_snowmen, :most_snowmen, :game_count
 end
